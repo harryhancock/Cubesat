cb9a3e36b7a992885eb9ebb433b1e024d0f37c9d
harryhancock <61349549+harryhancock@users.noreply.github.com>
2022-04-27 14:39:03 +0100

    Added source of code

--- pi_camera_setup.py
+++ pi_camera_setup.py
@@ -1,3 +1,5 @@
+#https://maker.pro/raspberry-pi/tutorial/how-to-interface-pi-noir-v2-camera-with-raspberry-pi
+
 from time import sleep
 from picamera import PiCamera
 camera = PiCamera() #initialise PiCamera object
